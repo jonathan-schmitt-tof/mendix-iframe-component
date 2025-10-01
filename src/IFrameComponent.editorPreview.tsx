668cef96a349addaeca34b14c743b5fcab28fa5c
Jonathan Schmitt <jonathan.schmitt@theorangeforce.us>
2025-10-01 16:05:33 -0600

    Cloned, upgraded all dependencies to 10.latest, tested.

--- src/IFrameComponent.editorPreview.tsx
+++ src/IFrameComponent.editorPreview.tsx
@@ -3,11 +3,9 @@ import { IFrameComponentPreviewProps } from "../typings/IFrameComponentProps";
 
 declare function require(name: string): string;
 
-const preview = (_props: IFrameComponentPreviewProps): ReactElement => {
+export const preview = (_props: IFrameComponentPreviewProps): ReactElement => {
     return <div />;
 };
-
-export default preview;
 
 export function getPreviewCss(): string {
     return require("./ui/IFrameComponent.scss");
--- typings/IFrameComponentProps.d.ts
+++ typings/IFrameComponentProps.d.ts
@@ -1,7 +1,7 @@
 /**
  * This file was generated from IFrameComponent.xml
  * WARNING: All changes made to this file will be overwritten
- * @author Mendix UI Content Team
+ * @author Mendix Widgets Framework Team
  */
 import { CSSProperties } from "react";
 import { ActionValue, DynamicValue } from "mendix";
@@ -31,8 +31,16 @@ export interface IFrameComponentContainerProps {
 }
 
 export interface IFrameComponentPreviewProps {
+    /**
+     * @deprecated Deprecated since version 9.18.0. Please use class property instead.
+     */
+    className: string;
     class: string;
     style: string;
+    styleObject?: CSSProperties;
+    readOnly: boolean;
+    renderMode: "design" | "xray" | "structure";
+    translate: (text: string) => string;
     urlExpression: string;
     srcExpression: string;
     uiSizeWidth: string;
